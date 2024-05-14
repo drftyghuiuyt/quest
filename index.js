2d7a4d4c160f3b205dab56cf5dd1ee12d089a13d
drftyghuiuyt <js1patdates@hotmail.com>
2024-05-15 06:16:21 +0700

    test project commit

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
-const selectionSort = (arr) => {
-  for (let i = 0; i < arr.length; i++) {
-    let minIndex = i;
-    for (let j = i + 1; j < arr.length; j++) {
-      if (arr[j] < arr[minIndex]) {
-        minIndex = j;
-      }
-    }
-    if (minIndex !== i) {
-      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
-    }
-  }
+const dropRightWhile = (arr, func) => {
+  while (arr.length > 0 && !func(arr[arr.length - 1])) arr = arr.slice(0, -1);
   return arr;
 };
